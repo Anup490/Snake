4c29eec7a439b302f3060e07e2a8c974b7670e49
Anup490 <anupbhandari490@gmail.com>
2020-08-15 13:44:34 +0545

    Code refactoring

--- snake/painter/painter.cpp
+++ snake/painter/painter.cpp
@@ -10,19 +10,8 @@ void Painter::paint() {
 	initRows(rows);
 	makeFrame(rows);
 	system("cls");
-	for (int i = 0; i < MAX_Y_AXIS; i++) {
-		if (rows[i].size > 0 ) {
-			for (int j = 0; j < rows[i].size; j++) {
-				bool glow = *(rows[i].pPointFlags + j);
-				(glow == true) ? cout << "*" : cout << " ";
-			}
-		}
-		cout << endl;
-	}
-	for (int i = 0; i < totalItems; i++) {
-		AbstractItem* pItem = pItems + i;
-		pItem->onDraw();
-	}
+	drawFrame(rows);
+	notifyItems();
 }
 
 void Painter::initRows(Row* pRows) {
@@ -38,11 +27,6 @@ void Painter::makeFrame(Row* pRows) {
 		Points* pPoints = pItem->getPoints();
 		for (int j = 0; j < pPoints->noOfPoints; j++) {
 			Point* pPoint = pPoints->pPointArray + j;
-			/*for (int j = 0; j < 6; j++) {
-				cout <<"(x = "<<(pPoint + j)->x << " , y = "<< (pPoint + j)->y<<") | ";
-			}
-			cout << endl;
-			cout << endl;*/
 			if (isWithinBounds(*pPoint)) {
 				Row* pRow = pRows + (pPoint->y);		
 				bool* pPointFlag = (pRow->pPointFlags) + (pPoint->x);
@@ -53,19 +37,27 @@ void Painter::makeFrame(Row* pRows) {
 			}
 		}
 	}
-	//////////////DRAW FRAMES (FOR TEST ONLY)//////////
-	/*for (int i = 0; i < MAX_Y_AXIS; i++) {
-		for (int j = 0; j < MAX_X_AXIS; j++) {
-			cout << *((pRows+i)->pPointFlags + j)<<" ";
-		}
-		cout << " | ROW SIZE = " << (pRows + i)->size;
-		cout << endl;
-	}
-	cout << endl;
-	cout << endl;*/
-	///////////////////////////////////////////////////
 }
 
 bool Painter::isWithinBounds(Point point) {
-	return (point.x < MAX_X_AXIS) && (point.y < MAX_Y_AXIS);
+	return (point.x < MAX_X_AXIS) && (point.x >= 0) && (point.y < MAX_Y_AXIS) && (point.y >= 0);
+}
+
+void Painter::drawFrame(Row* pRows) {
+	for (int i = 0; i < MAX_Y_AXIS; i++) {
+		if (pRows[i].size > 0) {
+			for (int j = 0; j < pRows[i].size; j++) {
+				bool glow = *(pRows[i].pPointFlags + j);
+				(glow == true) ? cout << "*" : cout << " ";
+			}
+		}
+		cout << endl;
+	}
+}
+
+void Painter::notifyItems() {
+	for (int i = 0; i < totalItems; i++) {
+		AbstractItem* pItem = pItems + i;
+		pItem->onDraw();
+	}
 }
--- snake/painter/painter.h
+++ snake/painter/painter.h
@@ -1,15 +1,10 @@
 #include <iostream>
 #include "../items/abstractItem.h"
-#include <list>
 
 #define TURN_LENGTH_X SNAKE_LENGTH
 #define TURN_LENGTH_Y SNAKE_LENGTH
 
 using namespace std;
-
-enum class Direction {
-	PX,PY,NX,NY,NONE
-};
 
 struct Row
 {
@@ -32,21 +27,11 @@ class Painter {
 	void initRows(Row* pRows);
 	void makeFrame(Row* pRows);
 	bool isWithinBounds(Point point);
-
-	//int spacesInX = 0;
-	//int spacesInY = 0;
-	//Direction previousDirection = Direction::PX;
-	//void paintInXAxis();
-	//void paintInYAxis();
-	//void changeSpacesInX();
-	//void changeSpacesInY();
-	//void printEmptySpaces();
+	void drawFrame(Row* pRows);
+	void notifyItems();
 public:
 	Painter(AbstractItem* pItems, int totalItems);
 	void paint();
-	
-	
-	//void paint(Direction direction);
 };
 
 
