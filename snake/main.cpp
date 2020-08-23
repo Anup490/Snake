6f504376b105e669f82e29664e5cfcfa5e34ad6d
Anup490 <anupbhandari490@gmail.com>
2020-08-23 08:22:54 +0545

    Code refactoring

--- snake/main.cpp
+++ snake/main.cpp
@@ -11,23 +11,10 @@ int main()
 	SnakeItem snakeItem;
 	Painter painter(&snakeItem,1);
 	Timer timer;
-	int i = 0;
 	int steps = 1;
-
 	while (steps < MAX_STEPS_FOR_ITERATION) {
 		if (timer.isMilliSecondComplete(INTERVAL_MILLIS)) {
-			if (steps > (MAX_X_AXIS * 2 + MAX_Y_AXIS + TURN_LENGTH_X)) {
-				painter.paint();
-			}
-			else if (steps > (MAX_X_AXIS + MAX_Y_AXIS)) {
-				painter.paint();
-			}
-			else if (steps > MAX_X_AXIS) {
-				painter.paint();
-			}
-			else {
-				painter.paint();
-			}
+			painter.paint();
 			steps++;
 		}
 	}
