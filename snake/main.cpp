cbad59317f3b7cd784c4c6741df04c48dbb051db
Anup490 <anupbhandari490@gmail.com>
2020-08-28 12:57:07 +0545

    Code refactoring

--- snake/main.cpp
+++ snake/main.cpp
@@ -6,6 +6,30 @@
 
 char value = 'l';
 
+void setupKeyListener(SnakeItem* pSnakeItem, Timer* pTimer);
+void listenInput(SnakeItem* pSnakeItem, Timer* pTimer);
+void runGame(Collision* pCollision, Painter* pPainter, Timer* pTimer);
+void showGameOverMessage();
+
+int main()
+{
+	SnakeItem snakeItem;
+	Painter painter(&snakeItem,1);
+	Timer timer;
+	Collision collision(SNAKE_LENGTH);
+	
+	setupKeyListener(&snakeItem, &timer);
+	runGame(&collision,&painter,&timer);
+	showGameOverMessage();
+	
+	return 0;
+}
+
+void setupKeyListener(SnakeItem* pSnakeItem, Timer* pTimer) {
+	thread listener(listenInput, pSnakeItem, pTimer);
+	listener.detach();
+}
+
 void listenInput(SnakeItem* pSnakeItem, Timer* pTimer) {
 	while (value != EXIT_KEY) {
 		if (pTimer->isMilliSecondComplete(FRAME_INTERVAL_MILLIS)) {
@@ -15,24 +39,22 @@ void listenInput(SnakeItem* pSnakeItem, Timer* pTimer) {
 	}
 }
 
-int main()
-{
-	SnakeItem snakeItem;
-	Painter painter(&snakeItem,1);
-	Timer timer;
-	Collision collision(SNAKE_LENGTH);
-	thread listener(listenInput, &snakeItem, &timer);
-	listener.detach();
+void runGame(Collision* pCollision, Painter* pPainter, Timer* pTimer) {
 	while (value != EXIT_KEY) {
-		if (timer.isMilliSecondComplete(FRAME_INTERVAL_MILLIS)) {
+		if (pTimer->isMilliSecondComplete(FRAME_INTERVAL_MILLIS)) {
 			try {
-				collision.checkCollision(painter.paint());
+				pCollision->checkCollision(pPainter->paint());
 			}
 			catch (CollisionException& e) {
 				value = EXIT_KEY;
 			}
 		}
 	}
-	return 0;
 }
 
+void showGameOverMessage() {
+	for (int i = 0; i < (((MAX_X_AXIS) / 2) - 5); i++) {
+		cout << " ";
+	}
+	cout << "GAME OVER" << endl;
+}
